fd779b6520c0f1d951b23ab5adae60ddee7c9a49
tawe141 <erictaw2018@u.northwestern.edu>
2015-11-20 14:10:01 -0600

    modal confirmed working

--- Laravel/resources/views/whatdata.blade.php
+++ Laravel/resources/views/whatdata.blade.php
@@ -1,13 +1,41 @@
 @extends('base')
 
 @section('content')
+
     <h1>Data</h1>
     @if(isset($author))
     <h2>From author: {{ $author }}</h2>
     @elseif(isset($tag))
     <h2>Tag: {{ $tag }}</h2>
     @endif
-    <div id="sortable">
+    <style>
+        .valign {
+            display:inline-block;
+            vertical-align:middle;
+            text-align: center;
+            float: none;
+            position: relative;
+            top: 50%;
+        }
+        .center {
+            margin-top: 1em;
+            padding-top: 1em;
+            padding-bottom: 1em;
+        }
+        .center-block {
+            margin: auto;
+            width: 30%;
+            padding: 10px;
+        }
+        .col-centered {
+            float: none;
+            margin: 0 auto;
+        }
+        .grayed {
+            color: #DDDDDD;
+        }
+    </style>
+    <div id="sortable" class="container">
         @foreach($data as $datum)
             <div class="col-md-3">
                 <h2><a href="{{ URL::to('whatdata/' . $datum->id) }}">Name: {{ $datum->name }}</a></h2>
@@ -17,5 +45,22 @@
                 <a class="btn btn-danger" href="{{ URL::to('/deleteinfo/' . $datum->id . '/confirm')}}">Delete</a>
             </div>
         @endforeach
+        <div class="col-md-3 center grayed" style="outline: #DDDDDD dotted thick" data-toggle="modal" data-target="#newStore">
+            <i class="glyphicon glyphicon-plus center-block" style="font-size: 5em"></i>
+            <h3 class="text-center">Click to add a new entry.</h3>
+        </div>
+        <!-- Modal -->
+        <div id="newStore" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal Content -->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4>Data Store</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @stop
