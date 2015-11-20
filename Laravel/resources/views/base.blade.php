8d4ebdd99a45e59e43b179cefa327faa44778639
tawe141 <erictaw2018@u.northwestern.edu>
2015-11-20 15:01:04 -0600

    working modal form

--- Laravel/resources/views/base.blade.php
+++ Laravel/resources/views/base.blade.php
@@ -2,7 +2,7 @@
 <html>
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.8/vue.min.js"></script>
+    <!-- <script src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.8/vue.min.js"></script> -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <head>
         <title>Laravel</title>
